165156f5a12bbab8823d5a7a0a678ca7dd45dcc6
Omar Rodríguez <omarr4061@gmail.com>
2024-04-11 11:53:40 -0600

    Primera subida de código

---
+++ README.md
@@ -0,0 +1,2 @@
+# CSD-P.A
+Repositorio donde se mantendrá un control de versiones para el código de P.A
---
+++ login-modular.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="navbar.css">
+    <link rel="stylesheet" href="login.css">
+</head>
+<style>
+    body {
+        background: linear-gradient(30deg, rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 15%, rgba(0, 212, 255, 1) 100%);
+        font-family: Calibri, sans-serif;
+    }
+</style>
+
+<body>
+
+    <!-- Navbar -->
+    <?php include 'navbar.html'; ?>
+
+    <!-- Login -->
+    <?php include 'login2.html'; ?>
+
+
+</body>
+
+</html>
