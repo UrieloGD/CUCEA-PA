6db2524093d46eaa6f8636117d26652b684dbf48
Cassandra Solis <catanaxvotos@gmail.com>
2024-05-07 12:18:12 -0500

    creación de rama plantilla-admin

---
+++ subirplantilla.php
@@ -0,0 +1,79 @@
+<!--header -->
+<?php include './template/header.php' ?>
+<!-- navbar -->
+<?php include './template/navbar.php' ?>
+<title>Plantilla</title>
+<link rel="stylesheet" href="./CSS/subirplantilla.css" />
+
+<!--Cuadro principal del home-->
+<div class="cuadro-principal">
+
+<!--Tabla Subir Plantilla-->
+<table>
+  <tr>
+    <th>Departamento</th>
+    <th>Archivo</th>
+    <th>Ultima Actualización</th>
+    <th>Acciones</th>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
