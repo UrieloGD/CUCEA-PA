5a88b7977da99e9859dae9c4ae28da9dbbd26b81
Cassandra Solis <catanaxvotos@gmail.com>
2024-06-16 14:34:08 -0500

    Se creo la base del Control de Eventos, falta formato

--- admin-eventos.php
+++ admin-eventos.php
@@ -20,21 +20,54 @@
     
 
 <!-- Contenido -->
-<head>
-    <title>Crear Evento</title>
-    <link rel="stylesheet" type="text/css" href="controlEv.css">
-</head>
-<body>
-    <h2>Crear Evento</h2>
-    <textarea id="event-description" placeholder="Escribe el nombre del evento"></textarea>
-</body>
+<div class="nombre-evento">
+        <p>Nombre del Evento</p>
+        <textarea id="event-description" placeholder="Escribe el nombre del evento"></textarea>
+    </div>
 
-<!-- <th style="text-align: center;">Nombre</th>
+    <div class="fecha">
+        <p>De</p>
+        <input type="text" id="event-description" placeholder="Junio 5, 2024"></input>
+        <p>a</p>
+        <input type="text" id="event-description" placeholder="Junio 5, 2024"></input>
+    </div>
 
+    <div class="participantes">
+        <p>Participantes</p>
+        <textarea id="event-description" placeholder="Escribe el nombre del participante"></textarea>
+    </div>
 
-<th style="text-align: center;">Participantes</th>
- Barra de Busqueda *comentado* 
-<div class="busqueda">
-  <input type="text" placeholder="Escribe el nombre del participante" id="search-input"> -->
+    <div class="notificaciones">
+        <p>Notificaciones</p>
+        <input type="number" value="1" min="1" max="9" style="width: 30px;">
+        <span>7</span>
+        <select>
+        <option>días</option>
+        <option>semanas</option>
+        <option>meses</option>
+        </select>
+        <span>antes a las</span>
+        <input type="time" value="19:00">
+        <input type="checkbox" id="correo-electronico">
+        <label for="correo-electronico">Correo electrónico</label>
+        <button>+ Nueva notificación</button>
+    </div>
+        
+
+    <div class="etiquetas">
+        <p>Etiquetas</p>
+        <button>+ Nueva Etiqueta</button>
+        <div>
+        <span>Jefes de Departamento</span>
+    <span>Coordinación de personal</span>
+    </div>
+</div>
+
+    <div class="descripcion">
+        <p>Descripción</p>
+        <textarea id="event-description" placeholder="Escriba la descripción de las actividades a realizar"></textarea>
+    </div>
+
+    </div>
 
 <?php include './template/footer.php' ?>
