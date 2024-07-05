33faad6a3c1c5174f3e5377ddace9d3a5c22228a
Uriel Valencia <urielvalencia799@gmail.com>
2024-07-05 15:15:03 -0600

    Modal nuevo para agregar registros en base de datos

--- basesdedatos.php
+++ basesdedatos.php
@@ -172,82 +172,104 @@ $result = mysqli_query($conexion, $sql);
 
 <!-- Modal para añadir registros -->
 <div id="modal-añadir" class="modal">
-    <div class="modal-content" style="max-width: 800px; margin: 5% auto;">
+    <div class="modal-content">
         <span class="close">&times;</span>
-        <h2>Añadir Nuevo Registro</h2>
-        <form id="form-añadir-registro" style="display: flex; flex-wrap: wrap; gap: 5px;">
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="ciclo" name="ciclo" placeholder="CICLO" required style="flex: 1;">
-                <input type="text" id="crn" name="crn" placeholder="CRN" required style="flex: 1;">
+        <h2>Añadir nuevo registro</h2>
+        <hr style="border: 1px solid #0071b0; width: 99%;">
+        <form id="form-añadir-registro">
+            <div class="form-container">
+                <div class="form-section">
+                    <h3>Materia</h3>
+                    <div class="form-row">
+                        <input type="text" id="ciclo" name="ciclo" placeholder="Ciclo">
+                        <input type="text" id="crn" name="crn" placeholder="CRN">
+                        <input type="text" id="cve_materia" name="cve_materia" placeholder="CVE Materia">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="materia" name="materia" placeholder="Materia" class="full-width">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="nivel" name="nivel" placeholder="Nivel">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="tipo" name="tipo" placeholder="Tipo">
+                        <input type="text" id="nivel_tipo" name="nivel_tipo" placeholder="Nivel tipo">
+                        <input type="text" id="seccion" name="seccion" placeholder="Sección">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="c_min" name="c_min" placeholder="C. Min" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                        <input type="text" id="h_totales" name="h_totales" placeholder="Horas totales" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                        <input type="text" id="estatus" name="estatus" placeholder="Status">
+                    </div>
+                    <div class="form-row weekdays">
+                        <input type="text" id="l" name="l" placeholder="L" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'L') this.value = '';">
+                        <input type="text" id="m" name="m" placeholder="M" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'M') this.value = '';">
+                        <input type="text" id="i" name="i" placeholder="I" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'I') this.value = '';">
+                        <input type="text" id="j" name="j" placeholder="J" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'J') this.value = '';">
+                        <input type="text" id="v" name="v" placeholder="V" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'V') this.value = '';">
+                        <input type="text" id="s" name="s" placeholder="S" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'S') this.value = '';">
+                        <input type="text" id="d" name="d" placeholder="D" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'D') this.value = '';">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="dia_presencial" name="dia_presencial" placeholder="Día presencial">
+                        <input type="text" id="dia_virtual" name="dia_virtual" placeholder="Día virtual">
+                        <input type="text" id="modalidad" name="modalidad" placeholder="Modalidad">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="fecha_inicial" name="fecha_inicial" placeholder="Fecha inicial">
+                        <input type="text" id="fecha_final" name="fecha_final" placeholder="Fecha final">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="hora_inicial" name="hora_inicial" placeholder="Hora inicial" maxlength="4" minlength="4" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                        <input type="text" id="hora_final" name="hora_final" placeholder="Hora final" maxlength="4" minlength="4" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="modulo" name="modulo" placeholder="Módulo">
+                        <input type="text" id="aula" name="aula" placeholder="Aula">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="cupo" name="cupo" placeholder="Cupo" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                        <input type="text" id="examen_extraordinario" name="examen_extraordinario" placeholder="Examen extraordinario">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="observaciones" name="observaciones" placeholder="Observaciones" class="full-width">
+                    </div>
+                </div>
+                <div class="form-section">
+                    <h3>Profesorado</h3>
+                    <div class="form-row">
+                        <input type="text" id="codigo_profesor" name="codigo_profesor" placeholder="Código Profesor" oninput="this.value = this.value.replace(/[^0-9]/g, '')" class="full-width">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="nombre_profesor" name="nombre_profesor" placeholder="Nombre profesor" class="full-width">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="tipo_contrato" name="tipo_contrato" placeholder="Tipo contrato">
+                        <input type="text" id="categoria" name="categoria" placeholder="Categoría">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="descarga" name="descarga" placeholder="Descarga" class="full-width">
+                        <input type="text" id="codigo_descarga" name="codigo_descarga" placeholder="Código descarga" class="full-width">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="nombre_descarga" name="nombre_descarga" placeholder="Nombre descarga" class="full-width">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="nombre_definitivo" name="nombre_definitivo" placeholder="Nombre definitivo" class="full-width">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="horas_totales" name="horas" placeholder="Horas totales" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                        <input type="text" id="titular" name="titular" placeholder="Titular">
+                    </div>
+                    <div class="form-row">
+                        <input type="text" id="horas" name="horas" placeholder="Horas" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
+                        <input type="text" id="codigo_dependencia" name="codigo_dependencia" placeholder="Código dependencia">
+                    </div>
+                </div>
             </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="materia" name="materia" placeholder="MATERIA" required style="flex: 3;">
-                <input type="text" id="cve_materia" name="cve_materia" placeholder="CVE MATERIA" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="seccion" name="seccion" placeholder="SECCIÓN" required style="flex: 1;">
-                <input type="text" id="nivel" name="nivel" placeholder="NIVEL" required style="flex: 1;">
-                <input type="text" id="nivel_tipo" name="nivel_tipo" placeholder="NIVEL TIPO" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="tipo" name="tipo" placeholder="TIPO" required style="flex: 1;">
-                <input type="text" id="c_min" name="c_min" placeholder="C. MIN" required style="flex: 1;">
-                <input type="text" id="h_totales" name="h_totales" placeholder="H. TOTALES" required style="flex: 1;">
-                <input type="text" id="estatus" name="estatus" placeholder="STATUS" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="tipo_contrato" name="tipo_contrato" placeholder="TIPO CONTRATO" required style="flex: 1;">
-                <input type="text" id="codigo_profesor" name="codigo_profesor" placeholder="CÓDIGO" required style="flex: 1;">
-            </div>
-            <input type="text" id="nombre_profesor" name="nombre_profesor" placeholder="NOMBRE PROFESOR" required style="width: 100%;">
-            <input type="text" id="categoria" name="categoria" placeholder="CATEGORIA" required style="width: 100%;">
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="descarga" name="descarga" placeholder="DESCARGA" required style="flex: 1;">
-                <input type="text" id="codigo_descarga" name="codigo_descarga" placeholder="CÓDIGO DESCARGA" required style="flex: 1;">
-                <input type="text" id="nombre_descarga" name="nombre_descarga" placeholder="NOMBRE DESCARGA" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="nombre_definitivo" name="nombre_definitivo" placeholder="NOMBRE DEFINITIVO" required style="flex: 1;">
-                <input type="text" id="titular" name="titular" placeholder="TITULAR" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="horas" name="horas" placeholder="HORAS" required style="flex: 1;">
-                <input type="text" id="codigo_dependencia" name="codigo_dependencia" placeholder="CÓDIGO DEPENDENCIA" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px; flex-wrap: wrap;">
-                <input type="text" id="l" name="l" placeholder="L" style="flex: 1; min-width: 40px;" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'L') this.value = '';">
-                <input type="text" id="m" name="m" placeholder="M" style="flex: 1; min-width: 40px;" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'M') this.value = '';">
-                <input type="text" id="i" name="i" placeholder="I" style="flex: 1; min-width: 40px;" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'I') this.value = '';">
-                <input type="text" id="j" name="j" placeholder="J" style="flex: 1; min-width: 40px;" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'J') this.value = '';">
-                <input type="text" id="v" name="v" placeholder="V" style="flex: 1; min-width: 40px;" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'V') this.value = '';">
-                <input type="text" id="s" name="s" placeholder="S" style="flex: 1; min-width: 40px;" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'S') this.value = '';">
-                <input type="text" id="d" name="d" placeholder="D" style="flex: 1; min-width: 40px;" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'D') this.value = '';">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="dia_presencial" name="dia_presencial" placeholder="DÍA PRESENCIAL" required style="flex: 1;">
-                <input type="text" id="dia_virtual" name="dia_virtual" placeholder="DÍA VIRTUAL" required style="flex: 1;">
-                <input type="text" id="modalidad" name="modalidad" placeholder="MODALIDAD" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="fecha_inicial" name="fecha_inicial" placeholder="FECHA INICIAL" required style="flex: 1;">
-                <input type="text" id="fecha_final" name="fecha_final" placeholder="FECHA FINAL" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="hora_inicial" name="hora_inicial" placeholder="HORA INICIAL" required style="flex: 1;">
-                <input type="text" id="hora_final" name="hora_final" placeholder="HORA FINAL" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="modulo" name="modulo" placeholder="MÓDULO" required style="flex: 1;">
-                <input type="text" id="aula" name="aula" placeholder="AULA" required style="flex: 1;">
-                <input type="text" id="cupo" name="cupo" placeholder="CUPO" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; gap: 5px;">
-                <input type="text" id="observaciones" name="observaciones" placeholder="OBSERVACIONES" required style="flex: 2;">
-                <input type="text" id="examen_extraordinario" name="examen_extraordinario" placeholder="EXAMEN EXTRAORDINARIO" required style="flex: 1;">
-            </div>
-            <div style="width: 100%; display: flex; justify-content: center; gap: 10px; margin-top: 10px;">
-                <button type="button" onclick="añadirRegistro()" style="padding: 5px 10px;">Añadir</button>
-                <button type="button" onclick="cerrarFormularioAñadir()" style="padding: 5px 10px;">Cancelar</button>
+            <div class="form-actions">
+                <button type="button" onclick="añadirRegistro()">Guardar</button>
+                <button type="button" onclick="cerrarFormularioAñadir()">Descartar</button>
             </div>
         </form>
     </div>
