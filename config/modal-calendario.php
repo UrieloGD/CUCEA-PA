57e8ea66d53193274fdf1a10c303aa12be034e0b
Omar Rodríguez <omarr4061@gmail.com>
2024-08-14 20:39:48 -0600

    Se limpió el código creando archivos independientes para manejar el JS y las funciones modales del calendario

---
+++ config/modal-calendario.php
@@ -0,0 +1,20 @@
+<!-- Modal -->
+<div id="eventModal" class="modal">
+    <div class="modal-content">
+        <div class="modal-header">
+            <span class="close">&times;</span>
+            <h2 id="eventTitle"></h2>
+        </div>
+        <div class="modal-body">
+            <div class="event-time">
+                <span id="eventDate"></span> • <span id="eventTime"></span>
+            </div>
+            <div class="event-location">
+                <span id="eventTag"></span>
+            </div>
+            <div class="event-description">
+                <p id="eventDescription"></p>
+            </div>
+        </div>
+    </div>
+</div>
