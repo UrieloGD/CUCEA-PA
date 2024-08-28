50c500e060be802fa9d8efca6d60cf8d8c5f0e75
Omar Rodriguez <omarr4061@gmail.com>
2024-08-28 15:51:03 -0600

    Se modificaron los campos del evento modal para añadir nuevos profesores

--- coordinacion_personal_profesores.php
+++ coordinacion_personal_profesores.php
@@ -84,6 +84,7 @@ $result = mysqli_query($conexion, $sql);
                 <th>HORARIOS NOMBRAMIENTO</th>
                 <th>TÉLEFONO</th>
                 <th>IMSS</th>
+
                 <th>RFC</th>
                 <th>CURP</th>
                 <th>CORREO</th>
@@ -127,68 +128,48 @@ $result = mysqli_query($conexion, $sql);
 <div id="modal-añadir" class="modal">
     <div class="modal-content">
         <span class="close">&times;</span>
-        <h2>Añadir nuevo registro</h2>
+        <h2>Añadir nuevo profesor</h2>
         <hr style="border: 1px solid #0071b0; width: 99%;">
         <form id="form-añadir-registro">
             <div class="form-container">
                 <div class="form-section">
-                    <h3>Materia</h3>
+                    <h3>Datos</h3>
                     <div class="form-row">
-                        <input type="text" id="ciclo" name="ciclo" placeholder="Ciclo">
-                        <input type="text" id="crn" name="crn" placeholder="CRN">
-                        <input type="text" id="cve_materia" name="cve_materia" placeholder="CVE Materia">
+                        <input type="text" id="nombre" name="nombre" placeholder="Nombre">
+                        <input type="text" id="apellido" name="apellido" placeholder="Apellido">
+                        <input type="text" id="edad" name="edad" placeholder="Edad">
                     </div>
                     <div class="form-row">
-                        <input type="text" id="materia" name="materia" placeholder="Materia" class="full-width">
+                        <input type="text" id="codigo" name="codigo" placeholder="Código">
+                        <input type="text" id="categoria" name="categoria" placeholder="Categoría">
+                        <input type="text" id="tipo_plaza" name="tipo_plaza" placeholder="Tipo de Plaza">
                     </div>
                     <div class="form-row">
-                        <input type="text" id="nivel" name="nivel" placeholder="Nivel">
+                        <input type="text" id="investigacion" name="investsigacion" placeholder="Investigación / Nombramiento / Cambio de función">
                     </div>
                     <div class="form-row">
-                        <input type="text" id="tipo" name="tipo" placeholder="Tipo">
-                        <input type="text" id="nivel_tipo" name="nivel_tipo" placeholder="Nivel tipo">
-                        <input type="text" id="seccion" name="seccion" placeholder="Sección">
+                        <input type="text" id="sni" name="sni" placeholder="SNI">
+                        <input type="text" id="cuando" name="cuando" placeholder="A partir de cuando">
+                        <input type="text" id="vencimiento" name="vencimiento" placeholder="Cuando se vence">
                     </div>
                     <div class="form-row">
-                        <input type="text" id="c_min" name="c_min" placeholder="C. Min" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
-                        <input type="text" id="h_totales" name="h_totales" placeholder="Horas totales" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
-                        <input type="text" id="estatus" name="estatus" placeholder="Status">
-                    </div>
-                    <div class="form-row weekdays">
-                        <input type="text" id="l" name="l" placeholder="L" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'L') this.value = '';">
-                        <input type="text" id="m" name="m" placeholder="M" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'M') this.value = '';">
-                        <input type="text" id="i" name="i" placeholder="I" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'I') this.value = '';">
-                        <input type="text" id="j" name="j" placeholder="J" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'J') this.value = '';">
-                        <input type="text" id="v" name="v" placeholder="V" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'V') this.value = '';">
-                        <input type="text" id="s" name="s" placeholder="S" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'S') this.value = '';">
-                        <input type="text" id="d" name="d" placeholder="D" maxlength="1" oninput="this.value = this.value.toUpperCase(); if(this.value != 'D') this.value = '';">
+                        <input type="text" id="horas_definitivas" name="horas_definitivas" placeholder="Horas Definitivas">
+                        <input type="text" id="horas_grupo" name="horas_grupo" placeholder="Horas frente a grupo">
+                        <input type="text" id="horarios_nombramiento" name="horarios_nombramiento" placeholder="Horas de nombramiento">
                     </div>
                     <div class="form-row">
-                        <input type="text" id="dia_presencial" name="dia_presencial" placeholder="Día presencial">
-                        <input type="text" id="dia_virtual" name="dia_virtual" placeholder="Día virtual">
-                        <input type="text" id="modalidad" name="modalidad" placeholder="Modalidad">
+                        <input type="text" id="tel" name="tel" placeholder="Télefono">
+                        <input type="text" id="imss" name="imss" placeholder="IMSS">
                     </div>
                     <div class="form-row">
-                        <input type="text" id="fecha_inicial" name="fecha_inicial" placeholder="Fecha inicial">
-                        <input type="text" id="fecha_final" name="fecha_final" placeholder="Fecha final">
+                        <input type="text" id="rfc" name="rfc" placeholder="RFC">
+                        <input type="text" id="curp" name="curp" placeholder="CURP">
                     </div>
                     <div class="form-row">
-                        <input type="text" id="hora_inicial" name="hora_inicial" placeholder="Hora inicial" maxlength="4" minlength="4" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
-                        <input type="text" id="hora_final" name="hora_final" placeholder="Hora final" maxlength="4" minlength="4" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
-                    </div>
-                    <div class="form-row">
-                        <input type="text" id="modulo" name="modulo" placeholder="Módulo">
-                        <input type="text" id="aula" name="aula" placeholder="Aula">
-                    </div>
-                    <div class="form-row">
-                        <input type="text" id="cupo" name="cupo" placeholder="Cupo" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
-                        <input type="text" id="examen_extraordinario" name="examen_extraordinario" placeholder="Examen extraordinario">
-                    </div>
-                    <div class="form-row">
-                        <input type="text" id="observaciones" name="observaciones" placeholder="Observaciones" class="full-width">
+                        <input type="email" id="email" name="email" placeholder="CORREO">
                     </div>
                 </div>
-                <div class="form-section">
+                <!-- <div class="form-section">
                     <h3>Profesorado</h3>
                     <div class="form-row">
                         <input type="text" id="codigo_profesor" name="codigo_profesor" placeholder="Código Profesor" oninput="this.value = this.value.replace(/[^0-9]/g, '')" class="full-width">
@@ -218,7 +199,7 @@ $result = mysqli_query($conexion, $sql);
                         <input type="text" id="horas" name="horas" placeholder="Horas" oninput="this.value = this.value.replace(/[^0-9]/g, '')">
                         <input type="text" id="codigo_dependencia" name="codigo_dependencia" placeholder="Código dependencia">
                     </div>
-                </div>
+                </div> -->
             </div>
             <div class="form-actions">
                 <button type="button" onclick="añadirRegistro()">Guardar</button>
