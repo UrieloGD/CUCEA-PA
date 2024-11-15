6856fd99dfcae37487f9819a33efcd2ce10e892f
Omar Rodríguez <omarr4061@gmail.com>
2024-11-15 13:39:50 -0600

    Se arregló error al intentar añadir registros a la tabla de coordinacion de personal

--- config/CrearBD.php
+++ config/CrearBD.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-include './../../config/db.php';
+include './../config/db.php';
 
 if (!$conexion) {
     die("Connection failed: " . mysqli_connect_error());
