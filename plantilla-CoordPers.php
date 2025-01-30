665fe64920a1feb94be089379783a119bea7da77
Omar Rodriguez <omarr4061@gmail.com>
2025-01-30 12:48:39 -0600

    Se agregaron restricciones para que solamente el rol Coordinación de Personal tenga acceso a las pestañas de Coordinación

--- plantilla-CoordPers.php
+++ plantilla-CoordPers.php
@@ -10,6 +10,14 @@ $fecha_limite = $row_fecha_limite ? $row_fecha_limite['Fecha_Limite'] : "2024-10
 
 ?>
 
+<?php
+// Verificar si el usuario está autenticado y tiene el Rol_ID correcto
+if (!isset($_SESSION['Codigo']) || $_SESSION['Rol_ID'] != 3) {
+    header("Location: home.php");
+    exit();
+}
+?>
+
 <!--header -->
 <?php include './template/header.php' ?>
 <!-- navbar -->
