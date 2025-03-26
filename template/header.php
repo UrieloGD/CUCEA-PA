d493d69f180b79782df4e5bdb1a695d904241431
Omar Rodriguez <omarr4061@gmail.com>
2025-03-26 09:20:33 -0600

    Se agregó un chat en vivo para que sirva de mesa de ayuda

--- template/header.php
+++ template/header.php
@@ -221,3 +221,17 @@ if ($rol_id == 1 || $rol_id == 2 || $rol_id == 3) { // Mostrar notificaciones pa
     }
   });
 </script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67e2f7a9f95a2519093a71d6/1in79erpc';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
